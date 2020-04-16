d598f9078f6131e787736fee0401d9cafd59b588
Micah White <micahjwhite@gmail.com>
2020-04-16 16:59:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,18 +7,16 @@ class Dog
     @this_dogs_name
   end
   
-  def bark=(dog_bark)
-    @this_dogs_bark = dog_bark
+  def bark(string)
+    puts "Woof!"
   end
   
-  def bark
-    @this_dogs_bark
-  end
+  
 end
 
 fido = Dog.new
 fido.name = "Fido"
-fido.bark = "Woof!"
+fido.bark 
 
 fido.name
 fido.bark
